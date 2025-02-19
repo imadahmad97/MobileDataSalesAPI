3bc413ff3bf2b1a2dd2b6b95746cf36cec68dfc3
Imad Ahmad <imad.ahmad@telus.com>
2025-02-19 15:10:22 -0800

    Removed testing files

--- appdata/bulk_upload_data/test_bulk.py
+++
@@ -1,8 +0,0 @@
-import csv
-import base64
-
-
-# Example usage
-file_path = r"C:\Users\t767284\Documents\repos\MobileDataSalesAPI\appdata\bulk_upload_data\bulk_upload_data.csv"  # Replace with your actual CSV file path
-decoded_data = parse_csv_to_dicts(file_path)
-print(decoded_data)
