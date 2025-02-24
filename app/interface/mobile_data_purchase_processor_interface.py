cbf7b9d590a3341a30aef544d94459581a161f39
Imad Ahmad <imad.ahmad@telus.com>
2025-02-23 18:20:08 -0800

    cleanup

--- app/interface/mobile_data_purchase_processor_interface.py
+++ app/interface/mobile_data_purchase_processor_interface.py
@@ -17,6 +17,7 @@ Methods:
 """
 
 import logging
+from fastapi import HTTPException
 from app.model.mobile_data_purchase_response import MobileDataPurchaseResponse
 from app.model.mobile_data_purchase_request import MobileDataPurchaseRequest
 from app.interface.validation_interface import validate_purchase_request
@@ -32,42 +33,48 @@ async def process_mobile_data_purchase_request(
     status of the request, records the request to the database, and generates a PDF invoice for the
     request.
     """
+    try:
+        logging.info("Initializing a new mobile data purchase response object")
+        # Prep Step: Initialize a new mobile data purchase response object:
+        purchase_response: MobileDataPurchaseResponse = MobileDataPurchaseResponse(
+            name=purchase_request.name,
+            credit_card_number=purchase_request.credit_card_number,
+            billing_account_number=purchase_request.billing_account_number,
+            requested_mobile_data=purchase_request.requested_mobile_data,
+            status="",
+            validation_errors="",
+        )
 
-    logging.info("Initializing a new mobile data purchase response object")
-    # Prep Step: Initialize a new mobile data purchase response object:
-    purchase_response: MobileDataPurchaseResponse = MobileDataPurchaseResponse(
-        name=purchase_request.name,
-        credit_card_number=purchase_request.credit_card_number,
-        billing_account_number=purchase_request.billing_account_number,
-        requested_mobile_data=purchase_request.requested_mobile_data,
-        status="",
-        validation_errors="",
-    )
+        logging.info("Validating the mobile data purchase request")
+        # Step 1: Validate the purchase request and append any validation errors to the response object
+        purchase_response.validation_errors += validate_purchase_request(
+            purchase_request.date_of_birth,  # type: ignore
+            purchase_request.credit_card_number,
+            purchase_request.credit_card_expiration_date,  # type: ignore
+            purchase_request.credit_card_cvv,
+        )
 
-    logging.info("Validating the mobile data purchase request")
-    # Step 1: Validate the purchase request and append any validation errors to the response object
-    purchase_response.validation_errors += validate_purchase_request(
-        purchase_request.date_of_birth,  # type: ignore
-        purchase_request.credit_card_number,
-        purchase_request.credit_card_expiration_date,  # type: ignore
-        purchase_request.credit_card_cvv,
-    )
+        logging.info("Updating the status of the mobile data purchase request")
+        # Step 2: Approve the purchase request if there are no validation errors
+        purchase_response.update_status()
 
-    logging.info("Updating the status of the mobile data purchase request")
-    # Step 2: Approve the purchase request if there are no validation errors
-    purchase_response.update_status()
+        logging.info("Recording the mobile data purchase request to the database")
+        # Step 3: Save the purchase request to the database
+        db_service.record_transaction(
+            purchase_request,
+            purchase_response.status,
+            purchase_response.validation_errors,
+        )
 
-    logging.info("Recording the mobile data purchase request to the database")
-    # Step 3: Save the purchase request to the database
-    db_service.record_transaction(
-        purchase_request,
-        purchase_response.status,
-        purchase_response.validation_errors,
-    )
+        logging.info("Generating a PDF invoice for the mobile data purchase request")
+        # Step 4: Generate a PDF invoice for the purchase request
+        generate_pdf_invoice(purchase_response)
 
-    logging.info("Generating a PDF invoice for the mobile data purchase request")
-    # Step 4: Generate a PDF invoice for the purchase request
-    generate_pdf_invoice(purchase_response)
-
-    # Return the purchase response object
-    return purchase_response
+        # Return the purchase response object
+        return purchase_response
+    except Exception:
+        logging.error("Failed to process the mobile data purchase request")
+        raise HTTPException(
+            status_code=500,
+            detail="Internal Server Error: Failed to process the mobile data purchase request",
+        )
--- app/service/api_request_handler.py
+++ app/service/api_request_handler.py
@@ -26,6 +26,7 @@ from app.model.mobile_data_purchase_response import MobileDataPurchaseResponse
 from app.interface.mobile_data_purchase_processor_interface import (
     process_mobile_data_purchase_request,
 )
+from fastapi import HTTPException
 
 
 async def handle_single_mobile_data_purchase_request(
@@ -36,28 +37,34 @@ async def handle_single_mobile_data_purchase_request(
     binary file, processes the request, and returns a JSON response with the status and BAN of the
     request.
     """
+    try:
+        logging.info("Building a mobile data purchase request from a binary file")
 
-    logging.info("Building a mobile data purchase request from a binary file")
-
-    purchase_request: MobileDataPurchaseRequest = (
-        await MobileDataPurchaseRequest.build_request_from_binary_file(
-            binary_purchase_request
+        purchase_request: MobileDataPurchaseRequest = (
+            await MobileDataPurchaseRequest.build_request_from_binary_file(
+                binary_purchase_request
+            )
         )
-    )
 
-    logging.info("Processing the mobile data purchase request")
+        logging.info("Processing the mobile data purchase request")
 
-    purchase_response: MobileDataPurchaseResponse = (
-        await process_mobile_data_purchase_request(purchase_request, db_service)
-    )
+        purchase_response: MobileDataPurchaseResponse = (
+            await process_mobile_data_purchase_request(purchase_request, db_service)
+        )
 
-    logging.info("Successfully processed the mobile data purchase request")
+        logging.info("Successfully processed the mobile data purchase request")
 
-    return JSONResponse(
-        content={
-            f"status for BAN {purchase_response.billing_account_number}": purchase_response.status
-        }
-    )
+        return JSONResponse(
+            content={
+                f"status for BAN {purchase_response.billing_account_number}": purchase_response.status
+            }
+        )
+    except Exception:
+        logging.error("Failed to process the mobile data purchase request")
+        raise HTTPException(
+            status_code=500,
+            detail="Internal Server Error: Failed to process the mobile data purchase request",
+        )
 
 
 async def handle_bulk_mobile_upload_purchase_request(
@@ -68,30 +75,38 @@ async def handle_bulk_mobile_upload_purchase_request(
     binary csv file, processes the requests, and returns a JSON response with the status and BAN
     of each request.
     """
-
-    logging.info(
-        "Building a list of mobile data purchase requests from a binary CSV file"
-    )
-
-    responses: list[MobileDataPurchaseResponse] = []
-
-    logging.info("Processing the mobile data purchase requests")
-
-    for row in MobileDataPurchaseRequest.build_request_list_from_binary_csv(  # type: ignore
-        csv_path
-    ):
-        responses.append(await process_mobile_data_purchase_request(row, db_service))
-
+    try:
         logging.info(
-            "Successfully processed a mobile data purchase request, BAN: %s",
-            row.billing_account_number,
+            "Building a list of mobile data purchase requests from a binary CSV file"
         )
 
-    logging.info("Successfully processed the bulk mobile data purchase requests")
+        responses: list[MobileDataPurchaseResponse] = []
 
-    return JSONResponse(
-        content={
-            f"status for BAN {response.billing_account_number}": response.status
-            for response in responses
-        }
-    )
+        logging.info("Processing the mobile data purchase requests")
+
+        for row in MobileDataPurchaseRequest.build_request_list_from_binary_csv(  # type: ignore
+            csv_path
+        ):
+            responses.append(
+                await process_mobile_data_purchase_request(row, db_service)
+            )
+
+            logging.info(
+                "Successfully processed a mobile data purchase request, BAN: %s",
+                row.billing_account_number,
+            )
+
+        logging.info("Successfully processed the bulk mobile data purchase requests")
+
+        return JSONResponse(
+            content={
+                f"status for BAN {response.billing_account_number}": response.status
+                for response in responses
+            }
+        )
+    except Exception:
+        logging.error("Failed to process the bulk mobile data purchase requests")
+        raise HTTPException(
+            status_code=500,
+            detail="Internal Server Error: Failed to process the bulk mobile data purchase requests",
+        )
