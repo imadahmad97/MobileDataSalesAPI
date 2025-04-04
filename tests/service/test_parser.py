4a3d28e5803bd879e9ffdc6e7fbe0493cadada03
Imad Ahmad <imad.ahmad@telus.com>
2025-04-04 13:38:32 -0700

    split parser tests

--- tests/service/test_parser.py
+++ tests/service/test_parser.py
@@ -1,13 +1,18 @@
 from app.service.parser import parse_csv_content, parse_text_from_binary
 from app.model.mobile_data_sell_order import MobileDataSellOrder
 
-bare_csv_content = b"header1,header2\nvalue1,value2\n"
+
 test_csv = b"John Doe,05/14/1990,406583246170089012345678,08/25,123,987654321,5GB\r\nDean Lawrence,11/7/1976,374245455400126,12/22,456,12349,1GB\r\nJane Doe,02/28/1985,374245455400126,08/25,123,988769,5GB\r\nJared Stevens,09/30/1982,374245455400126,12/25,456,432345,1GB\r\nRay Lopez,03/15/1995,406583246170089012345678,08/25,123,987654321,5GB\r\n"
 
 
-def test_parse_text_from_binary():
+def test_parse_text_from_binary_bare_csv():
+    bare_csv_content = b"header1,header2\nvalue1,value2\n"
     bare_csv_expected_result = [["header1", "header2"], ["value1", "value2"]]
+    bare_csv_actual_result = parse_text_from_binary(bare_csv_content)
+    assert bare_csv_actual_result == bare_csv_expected_result
 
+
+def test_parse_text_from_binary_csv_test_csv():
     test_csv_expected_result = [
         [
             "John Doe",
@@ -56,10 +61,7 @@ def test_parse_text_from_binary():
         ],
     ]
 
-    bare_csv_actual_result = parse_text_from_binary(bare_csv_content)
     test_csv_actual_result = parse_text_from_binary(test_csv)
-
-    assert bare_csv_actual_result == bare_csv_expected_result
     assert test_csv_actual_result == test_csv_expected_result
 
 
